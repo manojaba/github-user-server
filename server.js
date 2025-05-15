85aa555c3e6ce4787c54fc248843c363ab1124c8
manojaba <maanda.mannan@gmail.com>
2025-05-15 18:33:47 +0530

    / added

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/api/:username',(req,res) => {
       
 })
 
-app.get('*',(req,res) =>  {
+app.get('/*',(req,res) =>  {
     res.sendFile(path.join(__dirname,"./client/dist/index.html"))
 });
 
